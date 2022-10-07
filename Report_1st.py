028a555d9429026526144457da00737cdc7ad375
Rahyoyoung <112643307+Rahyoyoung@users.noreply.github.com>
2022-10-07 13:16:52 +0900

    Add files via upload

---
+++ Report_1st.py
@@ -0,0 +1,29 @@
+#1번
+print("-----1번-----")
+print("환영합니다.\n파이썬의 세계에 오신 것을 환영합니다. \n파이썬은 강력합니다.\n")
+#2번
+print("-----2번-----")
+print("반갑습니다. 파이썬!")
+print(2*3/10)
+print("Hello","World","!!!\n")
+#3번
+print("-----3번-----")
+print("한 주는",7*24,"시간이다.\n")
+#4번
+print("-----4번-----\n ---->")
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.mainloop()
